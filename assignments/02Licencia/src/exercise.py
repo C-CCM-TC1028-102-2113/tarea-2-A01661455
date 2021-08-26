cd8922c76cfefd331a6b47d3770bd998b56676e0
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-08-25 22:18:06 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,18 @@
 
 def main():
+    edad=int(input("Ingresa tu edad: "))
+    if edad>=18:
+        id=input("¿Tienes identificación oficial? (s/n)")
+        if "s" in id:
+            print("Trámite de licencia concedido")
+        elif "n" in id:
+             print("No cumples requisitos")
+        else:
+         print("Respuesta incorrecta")
+    else:
+        print("No cumples requisitos")
     #Escribe tu código debajo de esta línea
+
     pass
 
 
