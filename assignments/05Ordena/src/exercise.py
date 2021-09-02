b4a976f1d3fd8141d8eee2dcdf46feeb2e086536
A01661455 <89362416+A01661455@users.noreply.github.com>
2021-09-01 22:45:34 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -3,26 +3,15 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    if x<y and x<z:
-        print(x) 
-    elif x<y and x>z:
-        print(z)
-    else:
-        print(y)
-    
-    if y<x and y<z:
-        print(y) 
-    elif y<x and y>z:
-        print(z)
-    else:
-        print(x)
+    if x<y and x<z and y<z:
+        print(x,y,z) 
 
-    if z<y and z<x:
-        print(z) 
-    elif z<x and z>y:
-        print(y)
-    else:
-        print(x)
-        
+    elif y<x and y<z and x>z:
+        print(y,z,x) 
+    
+    elif z<y and z<x and y>x:
+        print(z,x,y) 
+    
 if __name__ == '__main__':
     main()
+
